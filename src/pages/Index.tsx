0ab65a10d65f2b34ebc4bf9d2a64f59aa4e5cb1e
SAUS INTERNATIONAL INC <darth3157@gmail.com>
2025-04-29 04:26:04 +0500

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -186,7 +186,7 @@ const Index = () => {
             </div>
             <div className="relative h-96 initially-hidden opacity-0">
               <img 
-                src="/lovable-uploads/093485ba-917c-4c11-940a-e82b453834fc.png?ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" 
+                src="/lovable-uploads/093485ba-917c-4c11-940a-e82b453834fc.png" 
                 alt="Construction team working" 
                 className="w-full h-full object-cover rounded-lg shadow-xl"
               />
