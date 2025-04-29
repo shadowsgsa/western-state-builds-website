b811d57bbff1284c61a250c25077f0d6906202d8
SAUS INTERNATIONAL INC <darth3157@gmail.com>
2025-04-29 23:26:55 +0500

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -139,15 +139,15 @@ const About = () => {
                 <p className="text-gray-600"></p>
                 <div className="grid grid-cols-1 sm:grid-cols-1 gap-4 mb-8">
                 <div className="flex items-start">
-                  <FaSearch className="h-5 w-5 text-construction-brick mr-2 mt-1" />
+                  <CheckCircle className="h-5 w-5 text-construction-brick mr-2 mt-1" />
                   <span>Our efficient workflows ensure that framing and foundation work is executed swiftly—without ever sacrificing quality.</span>
                 </div>
                 <div className="flex items-start">
-                  <FaBriefcase className="h-5 w-5 text-construction-brick mr-2 mt-1" />
+                  <CheckCircle className="h-5 w-5 text-construction-brick mr-2 mt-1" />
                   <span>Backed by years of hands-on industry experience, we’ve refined the techniques and craftsmanship needed to deliver durable, high-performance structures that stand the test of time.</span>
                 </div>
                 <div className="flex items-start">
-                  <FaHandHolding className="h-5 w-5 text-construction-brick mr-2 mt-1" />
+                  <CheckCircle className="h-5 w-5 text-construction-brick mr-2 mt-1" />
                   <span>From initial consultations to final inspections, we keep you informed every step of the way—so you always know exactly where your project stands.</span>
                 </div>
               </div>
