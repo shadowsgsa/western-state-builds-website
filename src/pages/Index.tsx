47bc984ff4b8769aaf86bc56125ae6d9d5b03ae7
SAUS INTERNATIONAL INC <darth3157@gmail.com>
2025-05-01 23:01:43 +0500

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -326,7 +326,7 @@ const Index = () => {
               <img 
                 src="/20250424_112047.png" 
                 alt="Construction team working" 
-                className="w-full h-full object-cover rounded-lg shadow-xl"
+                className="w-full h-full object-contain rounded-lg shadow-xl"
               />
               { // <div className="absolute -bottom-8 -left-8 bg-white p-4 rounded-lg shadow-lg w-40 h-40 flex flex-col items-center justify-center">
                // <span className="text-3xl font-bold text-construction-brick">25+</span>
